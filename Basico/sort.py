ea673a0d8e775ab9e20a1ba25175ec2e9ddff90a
DanielWPitsch <102161140+DanielWPitsch@users.noreply.github.com>
2023-04-21 08:49:46 -0300

    t2

---
+++ Basico/sort.py
@@ -0,0 +1,18 @@
+string = "1daniel "
+nome="eita"
+numero="eita22"
+lista=[1,2,3,4,5]
+
+lista[1]=3
+print(lista)
+
+string = ''.join(sorted(string))
+
+print(len(string))
+
+if(string.isalnum()):
+    print("Sim")
+else:
+    print("falso")
+
+#print(numero)
