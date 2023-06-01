c78cd327e77ab1eee28842e42f08c0e3fd7bff56
DanielWPitsch <102161140+DanielWPitsch@users.noreply.github.com>
2023-06-01 17:27:39 -0300

    obs

--- MatematicaDiscreta/analiseCombinatoria.py
+++ MatematicaDiscreta/analiseCombinatoria.py
@@ -14,13 +14,20 @@ def main():
     permutacoes = list(itertools.permutations(nome, 3))
     print("permutações: \n",permutacoes)
     print(len(permutacoes), "permutações\n")
+    
 
     #2 e 5.Permutação onde r < n, sem reposição 
     r= 2
     permutacaoR = fatorial(len(nome)) / fatorial(len(nome) - r)
     print("permutação r < n (sem reposição): \n", permutacaoR)
+    #r determina loops do for interno
+    #a variavel permutacaoR determina os loops do for externo
+    #cada loop interno insere 1 elemento numa lista
+    #cada loop externo insere uma lista na lista
+    #condição de não por elementos iguais
+    
 
-    #3.Quando os elementos podem se repetir
+    #3.Quando os elementos podem se repetir e r=n
     # fatorial(elementos) / fatorial(repetido1) * fatorial(repetido2)...
     palavra="banana"
     contador = Counter(palavra)
@@ -31,10 +38,14 @@ def main():
             divisor *= fatorial(contador[each])
     repetir= fatorial(len(palavra)) / divisor
     print("\npermutação quando os elementos podem se repetir: \n", repetir)
+
                    
     #4. Amostra com reposição = n^r
-    reposicao = pow(2, 3) #pow(n, r)
+    reposicao = pow(12, 3) #pow(n, r)
     print("\nAmostra com reposicao: \n", reposicao)
+    #smpre a mesma amostra repetindo r vezes
+    #quantas maneiras possiveis de reorganizar?
+    
 
     #6 Combinações
     combinacao = list(itertools.combinations(numeros, 2)) 
